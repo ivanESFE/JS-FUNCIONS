7c4fcd01666f6337f146ac30ddfedcbc500cd787
ivan02 <ivan.esteban@institutvidreres.cat>
2021-11-23 09:25:07 +0100

    Ex6-Funcions

--- JSFuncionsIvanEsteban.js
+++ JSFuncionsIvanEsteban.js
@@ -1,14 +1,11 @@
-//Ex-5 Funcions
+//Ex-6 Funcions
 
-function contarVocales(texto){
-    let contVocales = 0;
-    let vocales =['a','e','i','o','u'];
-
-    for(let i = 0; i< texto.length; ++i){
-        if (vocales.indexOf(texto[i]) >= 0) {
-            ++contVocales;
-        }
-    }
-    return contVocales;
+function numeros(){
+    let valor1 = prompt("Dame el valor 1",0);
+    let valor2 = prompt("Dame el valor 2",0);
+    let valor3 = 3
+    let suma = parseInt(valor1) + parseInt(valor2) + valor3;
+    console.log('La suma de ' + valor1 + ', ' + valor2 + ' i ' + valor3 + ' es: ' + suma)
 }
-console.log(contarVocales('catedral'));
+
+numeros();
