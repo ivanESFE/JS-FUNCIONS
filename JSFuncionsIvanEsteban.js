fc9756126954fb2aaa4be02c69bec06403681cb2
ivan02 <ivan.esteban@institutvidreres.cat>
2021-11-24 11:23:34 +0100

    Ex9-Funcions

--- JSFuncionsIvanEsteban.js
+++ JSFuncionsIvanEsteban.js
@@ -1,22 +1,17 @@
 //Ex-7 Funcions
 
-function culo(){
-    let valor1 = 15
-    let valor2 = 15
+function funcioAnonima(){
+    let valor1,valor2;
+    valor1=10
+    valor2=15
     if ((valor1 == 30)|| (valor2==30)|| valor1+valor2==30){
         
         console.log(true)
     }
-    
-   
-
-        
-     
-         
-        
-    
-    
+    else{
+        console.log(false)
+    }
 
 }
 
-culo();
+funcioAnonima();
