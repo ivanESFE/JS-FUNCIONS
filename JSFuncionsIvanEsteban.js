68336803644a96e457dc35b7518b0661c2a0ca6b
ivan02 <ivan.esteban@institutvidreres.cat>
2021-11-24 11:08:39 +0100

    Ex7-Funcions

--- JSFuncionsIvanEsteban.js
+++ JSFuncionsIvanEsteban.js
@@ -1,11 +1,22 @@
-//Ex-6 Funcions
+//Ex-7 Funcions
 
-function numeros(){
-    let valor1 = prompt("Dame el valor 1",0);
-    let valor2 = prompt("Dame el valor 2",0);
-    let valor3 = 3
-    let suma = parseInt(valor1) + parseInt(valor2) + valor3;
-    console.log('La suma de ' + valor1 + ', ' + valor2 + ' i ' + valor3 + ' es: ' + suma)
+function culo(){
+    let valor1 = 15
+    let valor2 = 15
+    if ((valor1 == 30)|| (valor2==30)|| valor1+valor2==30){
+        
+        console.log(true)
+    }
+    
+   
+
+        
+     
+         
+        
+    
+    
+
 }
 
-numeros();
+culo();
